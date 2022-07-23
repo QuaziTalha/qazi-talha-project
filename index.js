d650903fc79748891b6713d4196a123c4fde8038
QaziTalha4595 <97889460+QaziTalha4595@users.noreply.github.com>
2022-07-23 15:50:47 +0530

    modified:   index.js 	modified:   package-lock.json 	modified:   package.json 	new file:   public/index.html 	modified:   views/pages/index.ejs 	new file:   views/partials/card.ejs 	modified:   views/partials/nav.ejs

--- index.js
+++ index.js
@@ -3,11 +3,30 @@ const app = express();
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+var response="";
+var news="";
+
+const NewsAPI = require('newsapi');
+const newsapi = new NewsAPI('092330a1b5264930b1af7c9d7fecabe5');
+
+
+
+
 
   app.use(express.static(path.join(__dirname, 'public')));
   app.set('views', path.join(__dirname, 'views'));
   app.set('view engine', 'ejs');
-  app.get('/', (req, res) => res.render('pages/index'));
-  app.get('/apitest', (req, res) => res.send('hello') );
+  app.get('/', (req, res) =>
+  // res.redirect(url: 'public/index.js');
+  response.sendFile('public/index.html')
+  );
+  app.get('/apitest', (req, res) => 
+  newsapi.v2.topHeadlines({
+    language: 'en',
+  }).then(response => {
+    // console.log(response);
+    return res.send(response);
+  })
+  );
   app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
